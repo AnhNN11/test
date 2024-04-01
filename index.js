59315553457ba16c2ba30b1103cce0851511a043
AnhNN11 <Nguyennhatanh.dev@gmail.com>
2024-04-01 16:15:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.static("react-app/dist"));
+const port = 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
