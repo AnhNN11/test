aa1d6817307f279d501f33b4af2490508955b711
AnhNN11 <Nguyennhatanh.dev@gmail.com>
2024-04-01 16:43:33 +0700

    'second'

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.static("react-app/dist"));
-const port = 8080;
-app.listen(port, () => console.log(`Listening on port ${port}`));
